97600d286145be64c4b68045c958b6b2cb7a086f
Yasushi Ichikawa <ichikaway@gmail.com>
2019-06-23 10:44:50 +0900

    とりあえずテストパス

--- 8-merge-two-sorted-lists/1.php
+++ 8-merge-two-sorted-lists/1.php
@@ -16,5 +16,35 @@ class Solution {
      */
     function mergeTwoLists($l1, $l2) {
 
+		$result = $node = null;
+		while(true) {
+			if(is_null($l1) && is_null($l2)) {
+				return $result;
+			}
+			$val1 = $l1->val;
+			$val2 = $l2->val;
+
+			if( (is_null($l1) && !is_null($l2)) || ((!is_null($l1) && !is_null($l2)) && $val1 >= $val2)) {
+				if(is_null($result)) {
+					$node = new ListNode($val2);
+					$result = $node;
+				} else {
+					$node->next = new ListNode($val2);
+					$node= $node->next;
+				}
+				$l2 = $l2->next;
+			} else if( (is_null($l2) && !is_null($l1)) || ((!is_null($l1) && !is_null($l2)) && $val2 >= $val1)) {
+				if(is_null($result)) {
+					$node = new ListNode($val1);
+					$result = $node;
+				} else {
+					$node->next = new ListNode($val1);
+					$node= $node->next;
+				}
+				$l1 = $l1->next;
+			} else {
+				return $result;
+			}
+		}
     }
 }
